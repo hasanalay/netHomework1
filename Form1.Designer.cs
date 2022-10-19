4d57c5d5afa3db6bd2c450e83bdf1bcf690c2435
Hasan ALAY <hsn_aly@hotmail.com>
2022-10-19 18:28:58 +0300

    feat: add remove customer and employee methods

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,111 +51,114 @@
             // 
             // txt_lastname
             // 
-            this.txt_lastname.Location = new System.Drawing.Point(117, 121);
+            this.txt_lastname.Location = new System.Drawing.Point(89, 105);
             this.txt_lastname.Name = "txt_lastname";
             this.txt_lastname.Size = new System.Drawing.Size(123, 20);
-            this.txt_lastname.TabIndex = 23;
+            this.txt_lastname.TabIndex = 3;
             // 
             // txt_address
             // 
-            this.txt_address.Location = new System.Drawing.Point(117, 157);
+            this.txt_address.Location = new System.Drawing.Point(89, 138);
             this.txt_address.Name = "txt_address";
             this.txt_address.Size = new System.Drawing.Size(123, 20);
-            this.txt_address.TabIndex = 22;
+            this.txt_address.TabIndex = 4;
             // 
             // txt_name
             // 
-            this.txt_name.Location = new System.Drawing.Point(117, 92);
+            this.txt_name.Location = new System.Drawing.Point(89, 72);
             this.txt_name.Name = "txt_name";
             this.txt_name.Size = new System.Drawing.Size(123, 20);
-            this.txt_name.TabIndex = 21;
+            this.txt_name.TabIndex = 2;
             // 
             // txt_id
             // 
-            this.txt_id.Location = new System.Drawing.Point(117, 56);
+            this.txt_id.Location = new System.Drawing.Point(89, 39);
             this.txt_id.Name = "txt_id";
             this.txt_id.Size = new System.Drawing.Size(123, 20);
-            this.txt_id.TabIndex = 20;
+            this.txt_id.TabIndex = 1;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(303, 92);
+            this.listBox1.Location = new System.Drawing.Point(245, 72);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(448, 121);
+            this.listBox1.Size = new System.Drawing.Size(370, 82);
             this.listBox1.TabIndex = 19;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(616, 44);
+            this.comboBox2.Location = new System.Drawing.Point(494, 36);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 18;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(358, 47);
+            this.comboBox1.Location = new System.Drawing.Point(290, 36);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 17;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // btn_remove_employee
             // 
-            this.btn_remove_employee.Location = new System.Drawing.Point(616, 252);
+            this.btn_remove_employee.Location = new System.Drawing.Point(501, 172);
             this.btn_remove_employee.Name = "btn_remove_employee";
-            this.btn_remove_employee.Size = new System.Drawing.Size(114, 23);
-            this.btn_remove_employee.TabIndex = 15;
+            this.btn_remove_employee.Size = new System.Drawing.Size(115, 31);
+            this.btn_remove_employee.TabIndex = 10;
             this.btn_remove_employee.Text = "Remove Employee";
             this.btn_remove_employee.UseVisualStyleBackColor = true;
+            this.btn_remove_employee.Click += new System.EventHandler(this.btn_remove_employee_Click);
             // 
             // btn_list_employee
             // 
-            this.btn_list_employee.Location = new System.Drawing.Point(190, 313);
+            this.btn_list_employee.Location = new System.Drawing.Point(115, 206);
             this.btn_list_employee.Name = "btn_list_employee";
-            this.btn_list_employee.Size = new System.Drawing.Size(114, 26);
-            this.btn_list_employee.TabIndex = 16;
+            this.btn_list_employee.Size = new System.Drawing.Size(97, 31);
+            this.btn_list_employee.TabIndex = 8;
             this.btn_list_employee.Text = "List Employee";
             this.btn_list_employee.UseVisualStyleBackColor = true;
             this.btn_list_employee.Click += new System.EventHandler(this.btn_list_employee_Click);
             // 
             // btn_add_employee
             // 
-            this.btn_add_employee.Location = new System.Drawing.Point(47, 308);
+            this.btn_add_employee.Location = new System.Drawing.Point(14, 206);
             this.btn_add_employee.Name = "btn_add_employee";
-            this.btn_add_employee.Size = new System.Drawing.Size(114, 26);
-            this.btn_add_employee.TabIndex = 14;
+            this.btn_add_employee.Size = new System.Drawing.Size(97, 31);
+            this.btn_add_employee.TabIndex = 6;
             this.btn_add_employee.Text = "Add Employee";
             this.btn_add_employee.UseVisualStyleBackColor = true;
             this.btn_add_employee.Click += new System.EventHandler(this.btn_add_employee_Click);
             // 
             // btn_remove_customer
             // 
-            this.btn_remove_customer.Location = new System.Drawing.Point(365, 249);
+            this.btn_remove_customer.Location = new System.Drawing.Point(245, 172);
             this.btn_remove_customer.Name = "btn_remove_customer";
-            this.btn_remove_customer.Size = new System.Drawing.Size(114, 26);
-            this.btn_remove_customer.TabIndex = 13;
+            this.btn_remove_customer.Size = new System.Drawing.Size(115, 31);
+            this.btn_remove_customer.TabIndex = 9;
             this.btn_remove_customer.Text = "Remove Customer";
             this.btn_remove_customer.UseVisualStyleBackColor = true;
             this.btn_remove_customer.Click += new System.EventHandler(this.btn_remove_customer_Click);
             // 
             // btn_list_customer
             // 
-            this.btn_list_customer.Location = new System.Drawing.Point(190, 279);
+            this.btn_list_customer.Location = new System.Drawing.Point(115, 172);
             this.btn_list_customer.Name = "btn_list_customer";
-            this.btn_list_customer.Size = new System.Drawing.Size(114, 28);
-            this.btn_list_customer.TabIndex = 12;
+            this.btn_list_customer.Size = new System.Drawing.Size(97, 31);
+            this.btn_list_customer.TabIndex = 7;
             this.btn_list_customer.Text = "List Customer";
             this.btn_list_customer.UseVisualStyleBackColor = true;
             this.btn_list_customer.Click += new System.EventHandler(this.btn_list_customer_Click);
             // 
             // btn_add_customer
             // 
-            this.btn_add_customer.Location = new System.Drawing.Point(47, 279);
+            this.btn_add_customer.Location = new System.Drawing.Point(14, 172);
             this.btn_add_customer.Name = "btn_add_customer";
-            this.btn_add_customer.Size = new System.Drawing.Size(114, 23);
-            this.btn_add_customer.TabIndex = 11;
+            this.btn_add_customer.Size = new System.Drawing.Size(97, 31);
+            this.btn_add_customer.TabIndex = 5;
             this.btn_add_customer.Text = "Add Customer";
             this.btn_add_customer.UseVisualStyleBackColor = true;
             this.btn_add_customer.Click += new System.EventHandler(this.btn_add_customer_Click);
@@ -163,7 +166,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(541, 47);
+            this.label6.Location = new System.Drawing.Point(445, 39);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 13);
             this.label6.TabIndex = 9;
@@ -172,7 +175,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(300, 47);
+            this.label5.Location = new System.Drawing.Point(242, 39);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(42, 13);
             this.label5.TabIndex = 8;
@@ -181,7 +184,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(44, 157);
+            this.label4.Location = new System.Drawing.Point(16, 138);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 13);
             this.label4.TabIndex = 7;
@@ -190,7 +193,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(44, 121);
+            this.label3.Location = new System.Drawing.Point(16, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 6;
@@ -199,7 +202,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 92);
+            this.label2.Location = new System.Drawing.Point(16, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 10;
@@ -208,7 +211,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 56);
+            this.label1.Location = new System.Drawing.Point(16, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 5;
@@ -218,7 +221,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(639, 285);
             this.Controls.Add(this.txt_lastname);
             this.Controls.Add(this.txt_address);
             this.Controls.Add(this.txt_name);
@@ -240,6 +243,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -61,19 +61,69 @@ namespace homework
             foreach (IPerson x in customers)
                 comboBox1.Items.Add(x.Id);
         }
+        private void listCustomers()
+        {
+            listBox1.Items.Clear();
+            foreach (IPerson x in customers)
+                listBox1.Items.Add(x.Id + " " + x.FirstName + " " + x.LastName + " " + x.Address);
+            comboBox1.Items.Clear();
+            foreach (IPerson x in customers)
+                comboBox1.Items.Add(x.Id);
+        }
+
+        private void listEmployees()
+        {
+            listBox1.Items.Clear();
+            foreach (IPerson x in employees)
+                listBox1.Items.Add(x.Id + " " + x.FirstName + " " + x.LastName + " " + x.Address);
+            comboBox2.Items.Clear();
+            foreach (IPerson x in employees)
+                comboBox2.Items.Add(x.Id);
+        }
 
         private void btn_list_employee_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
             foreach (IPerson x in employees)
                 listBox1.Items.Add(x.Id + " " + x.FirstName + " " + x.LastName + " " + x.Address);
+            comboBox2.Items.Clear();
+            foreach (IPerson x in employees)
+                comboBox2.Items.Add(x.Id);
         }
 
 
         private void btn_remove_customer_Click(object sender, EventArgs e)
         {
-            customers.RemoveAll(x == x.Id == Convert.ToInt32(comboBox1.SelectedItem));
+            customers.Remove(customers[listBox1.SelectedIndex]);
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+            listCustomers();
 
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Customer customer = customers[comboBox1.SelectedIndex];
+            listBox1.Items.Add(Convert.ToString(customer.Id) + " " + customer.FirstName + " " + customer.LastName + " " + customer.Address);
+        }
+
+        private void btn_remove_employee_Click(object sender, EventArgs e)
+        {
+            employees.Remove(employees[listBox1.SelectedIndex]);
+            comboBox2.Items.Remove(comboBox2.SelectedItem);
+            listEmployees();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Employee employee = employees[comboBox2.SelectedIndex];
+            listBox1.Items.Add(Convert.ToString(employee.Id) + " " + employee.FirstName + " " + employee.LastName + " " + employee.Address);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
